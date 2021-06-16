d61c671c5bbe5bb1d62d39afc352955003f51716
Rogerio Cezar Fertonani <rogeriocezarfertonani@gmail.com>
2021-06-16 17:02:19 -0300

    projeto de um buscador de git

---
+++ lib/ui/homepage.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _offset = 0;
+  String _search = "";
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search.isEmpty) {
+      response = await http.get(
+        Uri.https('api.giphy.com', '/v1/gifs/trending', {
+          'api_key': 'BFAoH2iVvSGVsATTWMkNax2Fs1YW5rzR',
+          'limit': '25',
+          'rating': 'g'
+        }),
+      );
+    } else {
+      response = await http.get(Uri.https('api.giphy.com', '/v1/gifs/search', {
+        'api_key': 'BFAoH2iVvSGVsATTWMkNax2Fs1YW5rzR',
+        'q': _search,
+        'limit': '25',
+        'offset': _offset,
+        'rating': 'g',
+        'lang': 'en'
+      }));
+    }
+    return json.decode(response.body);
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _getGifs().then((map) => print(map));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: MediaQuery.of(context).size.height * 0.2,
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        elevation: 10,
+        shadowColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), labelText: "enter"),
+          )
+        ],
+      ),
+    );
+  }
+}
