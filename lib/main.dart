102812940bd3702663345e159e12f7aa37df1a48
Rogerio Cezar Fertonani <rogeriocezarfertonani@gmail.com>
2021-06-16 17:04:50 -0300

    atualizado a depencia http^0.2 para http^0.13.3  para evitar oerro NullSafe as variaveis que continham a http tiveram que ser reestruturadas.

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:buscador_gifs/ui/homepage.dart';
 
 void main() {
   runApp(MaterialApp(
+    theme: ThemeData(
+      hintColor: Colors.white,
+    ),
     home: HomePage(),
   ));
 }
